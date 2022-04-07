18e8a99a7025e39d065a98057992c874b19029ae
Elon <elon@fet.com>
2022-04-07 12:00:54 +0800

    add js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 Vue.createApp({
-    data() { return { courses: [] } },
-    computed: {},
-    methods: {}
-}).mount("#app")
+  data() {
+    return { courses: [], courseContent: "initial" };
+  },
+  computed: {},
+  methods: {
+    addCourse() {
+      this.courses.push(this.courseContent);
+    },
+  },
+}).mount("#app");
