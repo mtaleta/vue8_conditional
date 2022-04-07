0febbfd4ea458d45ec373375878bcccb24181fb5
Elon <elon@fet.com>
2022-04-07 13:25:46 +0800

    add for loop poop

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 Vue.createApp({
   data() {
-    return { courses: [], courseContent: "initial" };
+    return {
+      courses: [],
+      courseContent: "initial",
+      poop: {
+        name: "POOP",
+        duration: 35,
+        period: ["Sat", "Sun"],
+        location: ["Taipei", "Remote"],
+        instructor: {
+          name: "Mark",
+          location: "Taipei",
+        },
+      },
+    };
   },
   computed: {},
   methods: {
